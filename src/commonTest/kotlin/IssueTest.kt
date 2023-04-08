073910596233520b60f7e51dceb672a12011173f
Simone <simone.giacomelli@gmail.com>
2023-04-08 09:35:13 +0100

    improved repro

--- src/commonTest/kotlin/IssueTest.kt
+++ src/commonTest/kotlin/IssueTest.kt
@@ -1,69 +1,23 @@
-import kotlinx.serialization.*
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.elementDescriptors
-import kotlinx.serialization.descriptors.serialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.encoding.decodeStructure
-import kotlinx.serialization.encoding.encodeStructure
-import kotlinx.serialization.json.Json
-import kotlin.reflect.KClass
+import kotlinx.serialization.serializer
+import kotlin.reflect.typeOf
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class IssueTest {
+    /** This fails with jvm/1.8.20 */
     @Test
-    fun test() {
-        val column = Column(String::class, listOf<String?>(null))
-        Json.encodeToString(column)
+    fun test_issue() {
+        assertTrue(listOfNullable<String>().descriptor.elementDescriptors.first().isNullable)
     }
 
+    /** This works with all the combinations mentioned, i.e., js/jvm/1.8.20 and js/jvm/1.8.10 */
     @Test
-    fun test2() {
-        val isNullable = listOfNullable<String>().descriptor.elementDescriptors.first().isNullable
-        assertTrue(isNullable)
-    }
-
-    inline fun <reified T> listOfNullable(): KSerializer<List<Any?>> = serializer<List<T?>>() as KSerializer<List<Any?>>
-}
-
-object ColumnSerializer : KSerializer<Column> {
-    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Packet") {
-        element("dataType", serialDescriptor<String>())
-        element("payload", buildClassSerialDescriptor("payload"))
-    }
-
-    override fun serialize(encoder: Encoder, value: Column) {
-        val serializer = value.kClass.listSerializer
-        encoder.encodeStructure(descriptor) {
-            encodeStringElement(descriptor, 0, value.kClass.simpleName ?: error("?"))
-            encodeSerializableElement(descriptor, 1, serializer, value.payload)
-        }
-    }
-
-    override fun deserialize(decoder: Decoder): Column = decoder.decodeStructure(descriptor) {
-        error("no need to deserialize")
+    fun test_workaround() {
+        assertTrue(workaround<String>().descriptor.elementDescriptors.first().isNullable)
     }
 }
 
-@Serializable(with = ColumnSerializer::class)
-data class Column(val kClass: KClass<*>, val payload: List<Any?>)
-
-
-private val serializers: Map<KClass<*>, KSerializer<List<Any?>>> = mapOf(
-    pair<String>(),
-    pair<Int>(),
-    pair<Double>(),
-    pair<Boolean>(),
-)
-
-
-@Suppress("UNCHECKED_CAST")
-inline fun <reified T> pair(): Pair<KClass<*>, KSerializer<List<Any?>>> =
-    T::class to serializer<List<T?>>() as KSerializer<List<Any?>>
-
-
-val KClass<*>.listSerializer: KSerializer<List<Any?>>
-    get() = serializers[this]
-        ?: throw SerializationException("No registered serializer for class `${this.simpleName}`. Search for ${::serializers.name} and add it to the list.")
+inline fun <reified T> listOfNullable(): KSerializer<List<Any?>> = serializer<List<T?>>() as KSerializer<List<Any?>>
+inline fun <reified T> workaround(): KSerializer<List<Any?>> = serializer(typeOf<List<T?>>()) as KSerializer<List<Any?>>
